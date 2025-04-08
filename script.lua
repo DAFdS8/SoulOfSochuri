7c2fd79dabdfaaf992b80191a3f5cfbbbe0628d8
DAFdS8 <120821643+DAFdS8@users.noreply.github.com>
2025-04-07 22:15:44 -0300

    Adicionando script Lua inicial

---
+++ script.lua
@@ -0,0 +1,6 @@
+print("🌟 Bem-vindo ao poder do Sochuri, danado!")
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Sochuri Ativado!",
+    Text = "Seu script foi executado com sucesso 😈",
+    Duration = 5
+})
